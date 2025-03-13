b8d9803c169ad75c7a5fcdb5be7d05d5aa5cc22d
Hardik Patel <67120721+hardikkumarpatel@users.noreply.github.com>
2025-03-13 14:27:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ class App {
 
 const highlights = {
     "0": ["Lorem Ipsum", "Test", "1960s", "desktop"],  
-   
 };
+
+const highlights = [
+    "Lorem Ipsum", "Test", "1960s", "desktop",
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    "Duis aute irure dolor in reprehenderit in voluptate",
+    "A written directive requires 24-hour supervision of detainees by agency staff (including) a count of the detainee population at least once every eight hours and establishes procedures to ensure that the detainee is visually observed by agency staff at least every 30 minutes, and if audio and/or visual electronic surveillance equipment is used, a written directive specifies that the equipment will be controlled to reduce the possibility of invading a detainee’s personal privacy."
+];
+
 App.getHighlightPDFContent('input.pdf', 'output.pdf', highlights, [0, 0, 1]).then(console.log).catch(console.error);
